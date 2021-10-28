187ba019b8b1595e42680889b330c6b0b220098d
pstung2602 <tung.ps@beetechsoft.com>
2021-10-28 16:36:44 +0700

    api login use passport

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [ApiController::class, 'login'])->name('login');
+    Route::post('signup', [ApiController::class, 'signup']);
+
+//    Route::group([
+//        'middleware' => 'auth:api'
+//    ], function() {
+//        Route::get('logout', [ApiController::class, 'logout']);
+//        Route::get('user', [ApiController::class, 'user']);
+//    });
+//    Route::middleware(['api'])->group(function () {
+        Route::get('logout', [ApiController::class, 'logout']);
+        Route::get('user', [ApiController::class, 'user'])->middleware('auth');
+//    });
 });
